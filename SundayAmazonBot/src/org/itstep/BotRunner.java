e10075f9bdc761dcf8533a3a0b3ae71cf99183ec
Alexandr Gergalo <derien001@gmal.com>
2018-03-04 21:25:24 +0200

    Добавили логику запуска бота для проверки работоспособности

--- SundayAmazonBot/src/org/itstep/BotRunner.java
+++ SundayAmazonBot/src/org/itstep/BotRunner.java
@@ -1,8 +1,22 @@
 package org.itstep;
 
+import org.itstep.model.Account;
+import org.itstep.service.BotService;
+import org.openqa.selenium.WebDriver;
+
 public class BotRunner {
 
 	public static void main(String[] args) {
+		WebDriver driver = BotService.getFirefoxDriver();
+		
+		Account account = new Account("Gergalo_Alex_001@gmail.com", "123456789q", "Alex", "Gergalo");
+		driver = BotService.registerAccount(driver, account);
+		
+		if (driver != null) {
+			driver = BotService.addGoodToCart(driver, "");
+			driver.quit();
+		}
+		
 		
 	}
 
