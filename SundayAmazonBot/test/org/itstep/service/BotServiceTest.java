893c7d773749fabe8c187a1e68754d133b678dd0
Alexandr Gergalo <derien001@gmal.com>
2018-03-04 21:25:02 +0200

    Написали простой тейст инициализации драйвера

--- SundayAmazonBot/test/org/itstep/service/BotServiceTest.java
+++ SundayAmazonBot/test/org/itstep/service/BotServiceTest.java
@@ -3,12 +3,17 @@ package org.itstep.service;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 public class BotServiceTest {
 
 	@Test
 	public void testGetFirefoxDriver() {
-		fail("Not yet implemented");
+		WebDriver driver = BotService.getFirefoxDriver();
+		
+		assertNotNull(driver);
+		
+		driver.quit();
 	}
 
 }
