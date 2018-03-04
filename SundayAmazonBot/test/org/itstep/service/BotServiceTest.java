6d4614cc170488c3c8dfc261a25045ca81318c19
Alexandr Gergalo <derien001@gmal.com>
2018-03-04 11:30:23 +0200

    Добавили тест-кейс для нашего сервиса веб-драйвера

---
+++ SundayAmazonBot/test/org/itstep/service/BotServiceTest.java
@@ -0,0 +1,14 @@
+package org.itstep.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BotServiceTest {
+
+	@Test
+	public void testGetFirefoxDriver() {
+		fail("Not yet implemented");
+	}
+
+}
