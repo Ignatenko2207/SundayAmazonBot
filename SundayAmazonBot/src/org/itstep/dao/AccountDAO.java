43cb5164e9b701b3da4ad9eb5412a2efedb97e1c
Alexandr Gergalo <derien001@gmal.com>
2018-03-11 11:45:39 +0200

    Создали DAO класс для доступа к БД - таблица аккаунтов

---
+++ SundayAmazonBot/src/org/itstep/dao/AccountDAO.java
@@ -0,0 +1,32 @@
+package org.itstep.dao;
+
+import java.util.List;
+
+import org.itstep.model.Account;
+
+public class AccountDAO {
+
+	public Account save(Account account) {
+
+		return null;
+	}
+
+	public Account get(String eMail) {
+
+		return null;
+	}
+	
+	public List<Account> getAll() {
+
+		return null;
+	}
+
+	public Account update(Account account) {
+
+		return null;
+	}
+
+	public void delete(Account account) {
+
+	}
+}
